899bdcfb6b465c8de7a90783eb105e2d046b2bc5
Trentcrypt3d <70151180+Trentcrypt3d@users.noreply.github.com>
2024-03-12 12:54:54 +0800

    added new page on landing page

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,14 +1,22 @@
 import React from "react";
-import CompanyPage from "./MembersPage/CompanyPage";
+import MembersPage from "./MembersPage/MembersPage.jsx";
 import DiscoverPage from "./DiscoverPage/DiscoverPage";
+import TeamPage from "./TeamCompanyPage/TeamPage";
+import CompanyPage from "./Company Page/CompanyPage.jsx";
+import FAQPage from "./FAQ/FAQPage.jsx";
+import Footer from "../components/Footer/Footer.jsx";
 
 function LandingPage() {
   return (
     <>
       {/*Main Landing Page*/}
-      <div className="flex flex-col w-full">
-        <CompanyPage />
+      <div className="flex flex-col w-full bg-discoverWhite">
+        <MembersPage />
         <DiscoverPage />
+        <TeamPage />
+        <CompanyPage />
+        <FAQPage />
+        <Footer />
       </div>
     </>
   );
