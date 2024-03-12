55fb4758c36ddb3bf68718ea20ecc3e9d1396c52
Trentcrypt3d <70151180+Trentcrypt3d@users.noreply.github.com>
2024-03-12 12:55:25 +0800

    added company page

---
+++ src/pages/Company Page/CompanyData.jsx
@@ -0,0 +1,18 @@
+const CompanyContent = [
+  {
+    id: "1",
+    companyDescription:
+      "BiNNO, founded by IT students from Bicol University in 2023, is an accessible and user-friendly platform designed to empower startup enthusiasts in the Bicol Region. We provide a variety of knowledge, connecting startup enablers and companies to share expertise and insights from their diverse fields.",
+    mission:
+      "To empower and catalyze innovation and entrepreneurship in the Bicol Region, our mission at BiNNO is to foster a collaborative ecosystem through the BiCoRSE consortium. We strive to provide a dynamic online platform that connects aspiring startup entrepreneurs, established companies, and startup enablers, facilitating knowledge exchange, resource sharing, and community engagement. By leveraging technology and inclusivity, we aim to create opportunities that drive economic growth, technological innovation, and societal development in the Bicol Region and beyond.",
+    vision:
+      "As the pioneering force in the Bicol startup ecosystem, BiNNO envisions a thriving community where every entrepreneur has the tools, knowledge, and support needed to transform innovative ideas into successful ventures. Our vision is to be the catalyst that propels the Bicol Region into a hub of creativity, economic prosperity, and sustainable development. Through the BiCoRSE consortium, we aim to build bridges, break barriers, and inspire a new generation of entrepreneurs who will shape the future of our region and contribute to the global startup landscape.",
+    images: [
+      "https://www.wework.com/ideas/wp-content/uploads/sites/4/2017/06/Web_150DPI-20190927_10th_Floor_Conference_Room_2_v1-1120x630.jpg",
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ3m-bp-6lpiR9hMIK_v9T3nuM2zIEJTUIzOXfZCyXigA&s",
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ1s_LHTasUt8xfbctBtf6rx_i3rmBWeplMGIYbTRdxRQ&s",
+    ],
+  },
+];
+
+export default CompanyContent;
---
+++ src/pages/Company Page/CompanyPage.jsx
@@ -0,0 +1,65 @@
+import React from "react";
+import CompanyContent from "./CompanyData.jsx";
+
+function CompanyPage() {
+  return (
+    <>
+      {/* company page */}
+      {CompanyContent.map((val) => (
+        <div
+          className="flex flex-col bg-discoverWhite items-center h-auto"
+          key={val.id}
+        >
+          <div className="absolute">
+            <h1 className="text-9xl bg-gradient-to-r from-discoverWhite from-50% to-companyBG to-50% font-bold p-5 bg-clip-text text-transparent">
+              Company
+            </h1>
+          </div>
+          <div className="grid grid-cols-2 bg-discoverWhite">
+            {/* company images */}
+            <div className="bg-companyBG flex justify-center h-auto w-full p-10">
+              {/* <h1 className="text-9xl text-white">Company</h1> */}
+              <div className="flex flex-col items-center mt-40">
+                <img
+                  className="object-cover w-96 h-60 m-4 rounded"
+                  src="https://assets.entrepreneur.com/content/3x2/2000/20150805204041-google-company-building-corporate.jpeg"
+                  alt=""
+                />
+                <img
+                  className="object-cover w-96 h-60 m-4 rounded"
+                  src="https://assets.entrepreneur.com/content/3x2/2000/20150805204041-google-company-building-corporate.jpeg"
+                  alt=""
+                />
+                <img
+                  className="object-cover w-96 h-60 m-4 rounded"
+                  src="https://assets.entrepreneur.com/content/3x2/2000/20150805204041-google-company-building-corporate.jpeg"
+                  alt=""
+                />
+                <img
+                  className="object-cover w-96 h-60 m-4 rounded"
+                  src="https://assets.entrepreneur.com/content/3x2/2000/20150805204041-google-company-building-corporate.jpeg"
+                  alt=""
+                />
+              </div>
+            </div>
+            {/* description page */}
+            <div className="bg-discoverWhite m-10 h-auto">
+              <div className="flex flex-col items-center justify-center p-5 mt-40">
+                <p className="text-black text-2xl">{val.companyDescription}</p>
+                {/* mission vision container */}
+                <div className="flex flex-col justify-center items-center">
+                  <h1 className="text-CustomOrange text-5xl m-10">Mission</h1>
+                  <p className="text-black text-2xl">{val.mission}</p>
+                  <h1 className="text-CustomOrange text-5xl m-10 ">Vision</h1>
+                  <p className="text-black text-2xl">{val.vision}</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      ))}
+    </>
+  );
+}
+
+export default CompanyPage;
