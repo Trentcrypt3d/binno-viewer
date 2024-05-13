9a6a00189068b12be41ce4bd989e5f47fc850ac6
Ross Marco Cordova <114625981+Rossmmc@users.noreply.github.com>
2024-05-14 02:35:56 +0800

    FIXED TEAM PAGE

--- src/pages/Company Page/Carousel_Swiper_Team.jsx
+++ src/pages/Company Page/Carousel_Swiper_Team.jsx
@@ -30,40 +30,19 @@ function CarouselTeam() {
 
   return (
     <Swiper
-      slidesPerView={4}
+      slidesPerView={3}
       loop={true}
       autoplay={{
-        delay: 1000,
+        delay: 2000,
         disableOnInteraction: false,
       }}
       grabCursor={false}
-      centeredSlides={true}
-      spaceBetween={-800}
+      centeredSlides={false}
+      spaceBetween={-600}
      
       navigation={true}
       onSlideChange={handleSlideChange}
-      breakpoints={{
-        "@0.00": {
-          slidesPerView: 5,
-          spaceBetween: 0,
-        },
-        "@0.67": {
-          slidesPerView: 5,
-          spaceBetween: 0,
-        },
-        "@0.75": {
-          slidesPerView: 5,
-          spaceBetween: 0,
-        },
-        "@1.00": {
-          slidesPerView: 5,
-          spaceBetween: 0,
-        },
-        "@1.50": {
-          slidesPerView: 5,
-          spaceBetween: 0,
-        },
-      }}
+    
       modules={[Autoplay, Pagination, Navigation]}
       className="TeamSwiper"
     >
@@ -76,20 +55,21 @@ function CarouselTeam() {
                 {/* card container (parent container)*/}
                 <div className="flex flex-col justify-center items-center max-h-full max-w-full">
                   <img
-                   className={`flex h-full max-h-full w-full object-cover object-center rounded 
-                   ${isDone(index, activeIndex, CarouselTeamData.length) ? 'done' : ''}
+                   className={`flex h-full max-h-full w-full object-cover object-center rounded-xl 
+                   ${isDone(index, activeIndex+2, CarouselTeamData.length) ? 'done' : ''}
+                   ${isDone(index, activeIndex+3, CarouselTeamData.length) ? 'donea' : ''}
                    ${isFit(index, activeIndex, CarouselTeamData.length) ? 'fit' : ''}
-                   ${index !== (activeIndex) % CarouselTeamData.length ? 'non-active' : ''}`
+                   ${index !== (activeIndex+2) % CarouselTeamData.length ? 'non-active' : ''}`
                  }
                     src={team.profileCover}
                     alt="Profile Cover"
                   />
                   {/* content container */}
-                  <div className={`flex justify-center items-center flex-col ml-2 overflow-visible bg-companyBG p-2 px-4 opacity-80 rounded-xl ${index !== (activeIndex) % CarouselTeamData.length ? 'hidden' : ''}`}>
-                    <div className="flex  font-bold text-sm text-orange-800">
+                  <div className={`flex justify-center items-center flex-col ml-28 overflow-visible p-2 mt-[-20px] ${index !== (activeIndex+2) % CarouselTeamData.length ? 'hidden' : ''}`}>
+                    <div className="flex  font-bold text-2xl text-orange-800">
                       {team.name}
                     </div>
-                    <div className="flex text-white text-sm">{team.role}</div>
+                    <div className="flex text-slate-500 text-md">{team.role}</div>
                   </div>
                 </div>
               </div>
--- src/pages/TeamCompanyPage/TeamPage.jsx
+++ src/pages/TeamCompanyPage/TeamPage.jsx
@@ -6,13 +6,13 @@ function TeamPage() {
     <div className="flex h-screen w-screen relative ">
       <div className="w-6/12 h-full bg-discoverwhite"></div>
       <div className="w-6/12 h-full bg-companyBG"></div>
-      <div className="absolute flex flex-col w-full p-24 pt-0">
-      <div className="relative w-full flex justify-center items-center">
-        <h1 className="text-5xl md:text-6xl lg:text-7xl xl:text-8xl 2xl:text-9xl bg-gradient-to-r from-companyBG from-50% to-discoverWhite to-30% font-bold m-5 bg-clip-text text-transparent p-12 mt-12">
+      <div className="absolute flex flex-col w-full pt-0 mt-0">
+      <div className="relative w-full flex justify-start ml-6 items-center">
+        <h1 className="text-5xl md:text-6xl lg:text-7xl xl:text-8xl 2xl:text-9xl bg-gradient-to-r from-companyBG from-[73%] to-discoverWhite to-30% font-bold m-5 bg-clip-text text-transparent p-12 mt-4">
           Meet the Team
         </h1>
       </div>
-      <div className=" relative left-0 bottom-0 w-full h-96 flex items-end">
+      <div className=" relative left-0 bottom-0 w-full h-96 flex items-end mt-32">
         <CarouselTeam />
       </div>
       </div>
